fc9bf030a1b05e00cc2c4e90acd224ac4a13f891
Jayoola <105334058+Jayoola@users.noreply.github.com>
2022-05-28 12:20:48 +0100

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,39 @@
+from multiprocessing import set_forkserver_preload
+
+
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, track,score):
+        self.name = name
+        self.age = age
+        self.tracks = track
+        self.score = score
+
+print("Details of new student:")
+def change_name(self, new_name):
+    self.name = new_name
+    print("Name: ", self.name)
+
+def change_age(self, new_age):
+    self.age = new_age
+    print("Age: ", self.age)
+
+def add_track(self, new_track):
+    self.tracks = self.tracks.append()
+    print("Tracks: ", self.tracks)
+
+def get_score(self):
+    print("Score: ", self.score)
+
+    
+
+
+
+Bob = Student(name="Bob", age=26, track=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
+
